7c18b76387ed0e6abf2e753dceaa0de16a8c583e
Islam Kambarov <ikambarov@yahoo.com>
2020-08-29 18:05:36 +0000

    Fixing the bad idea

--- vpc.tf
+++ vpc.tf
@@ -44,8 +44,8 @@ variable "az3" {
 module "vpc" {
   source = "./modules/vpc"
 
-  environment               = "var.environment"
-  vpc_cidr_block            = "var.vpc_cidr_block"
+  environment               = var.environment
+  vpc_cidr_block            = var.vpc_cidr_block
   availability_zones        = [ var.az1, var.az2, var.az3 ]
   public_subnet_cidr_blocks = [ var.public_cidr1, var.public_cidr2, var.public_cidr3 ]
   private_subnet_cidr_blocks = [ var.private_cidr1, var.private_cidr2, var.private_cidr3 ]
