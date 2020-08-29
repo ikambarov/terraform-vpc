5de18062903f2aa1fd3a7ba4d065672abcd511ca
Islam Kambarov <ikambarov@yahoo.com>
2020-08-29 17:13:44 +0000

    Create more vars

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,33 @@
+variable "environment" {
+  type    = string
+}
+
+variable "vpc_cidr_block" {
+  type    = string
+}
+
+variable "public_cidr1" {
+  type    = string
+}
+
+variable "public_cidr2" {
+  type    = string
+}
+variable "public_cidr3" {
+  type    = string
+}
+
+variable "private_cidr1" {
+  type    = string
+}
+
+variable "private_cidr2" {
+  type    = string
+}
+variable "private_cidr3" {
+  type    = string
+}
+
 variable "public_subnet_cidr_blocks" {
   type = list
   default = [
@@ -25,13 +55,7 @@ variable "availability_zones" {
   ]
 }
 
-variable "environment" {
-  type    = string
-}
 
-variable "vpc_cidr_block" {
-  type    = string
-}
 
 module "vpc" {
   source = "./modules/vpc"
