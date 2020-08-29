87612c6a94b3441e572367d1b4dd9c0dff138bdd
Islam Kambarov <ikambarov@yahoo.com>
2020-08-29 17:29:51 +0000

    Fix lists

--- vpc.tf
+++ vpc.tf
@@ -41,43 +41,14 @@ variable "az3" {
   type    = string
 }
 
-variable "public_subnet_cidr_blocks" {
-  type = list
-  default = [
-    var.public_cidr1,
-    var.public_cidr2,
-    var.public_cidr3,
-  ]
-}
-
-variable "private_subnet_cidr_blocks" {
-  type = list
-  default = [
-    var.private_cidr1,
-    var.private_cidr2,
-    var.private_cidr3,
-  ]
-}
-
-variable "availability_zones" {
-  type = list
-  default = [
-    var.az1,
-    var.az2,
-    var.az3,
-  ]
-}
-
-
-
 module "vpc" {
   source = "./modules/vpc"
 
-  availability_zones        = var.availability_zones
   environment               = var.environment
   vpc_cidr_block            = var.vpc_cidr_block
-  public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
-  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
+  availability_zones        = [ var.az1, var.az2, var.az3 ]
+  public_subnet_cidr_blocks = [ var.public_cidr1, var.public_cidr2, var.public_cidr3 ]
+  private_subnet_cidr_blocks = [ var.private_cidr1, var.private_cidr2, var.private_cidr3 ]
 }
 
 output "vpc_id" {
