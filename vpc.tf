64c9ed31325b594f0450bd946bc64b6d20ac4bc8
Islam Kambarov <ikambarov@yahoo.com>
2020-08-29 17:19:17 +0000

    Fix missing var

--- vpc.tf
+++ vpc.tf
@@ -77,6 +77,7 @@ module "vpc" {
   environment               = var.environment
   vpc_cidr_block            = var.vpc_cidr_block
   public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
+  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
 }
 
 output "vpc_id" {
